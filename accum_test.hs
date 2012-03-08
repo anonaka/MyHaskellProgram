4e17ac3136bb2ed904bb15af64d503254668ede1
Your Name <nonaka@mac.com>
2012-03-08 17:41:32 +0900

    added main body.

--- accum_test.hs
+++ accum_test.hs
@@ -1,9 +1,12 @@
 a = [1,2,3,4,5] :: [Int]
 
-myaccum :: [Int] -> [Int] -> [Int]
-myaccum [] rslt = reverse rslt
-myaccum (x:xs) [] = myaccum xs [x]
-myaccum (x:xs) rslt = myaccum xs ((x + head rslt):rslt)
+myaccum :: [Int] -> [Int]
+myaccum xs = myaccum_it xs []
+
+myaccum_it :: [Int] -> [Int] -> [Int]
+myaccum_it [] rslt = reverse rslt
+myaccum_it (x:xs) [] = myaccum_it xs [x]
+myaccum_it (x:xs) rslt = myaccum_it xs ((x + head rslt):rslt)
        
 myaccum2 :: [Int] -> [Int]
 myaccum2 x = reverse $ foldl fun1 [] x
