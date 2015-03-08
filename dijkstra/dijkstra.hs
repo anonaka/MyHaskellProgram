3a291f11bcb555211047e65df02e715025bfbb02
Akira Nonaka <nonaka@mac.com>
2015-03-08 15:09:38 +0900

    added findShortestDistanceNode

--- dijkstra/dijkstra.hs
+++ dijkstra/dijkstra.hs
@@ -6,19 +6,27 @@ type Distance = Int
 data Node = Node NodeId NodeLabel Distance deriving (Show,Eq)
 data Edge = Edge Node Node EdgeLength deriving (Show,Eq)
 
+instance Ord Node where
+    compare (Node _ _ d) (Node _ _ d')
+        | d == d' = EQ
+        | d < d' = LT
+        | otherwise = GT
+
 instance Ord Edge where
-  compare (Edge n1 n2 len) (Edge n1' n2' len')
-    | len == len'   = EQ
-    | len < len'    = LT
-    | otherwise = GT
+    compare (Edge _ _ len) (Edge _ _ len')
+        | len == len'   = EQ
+        | len < len'    = LT
+        | otherwise = GT
 
 
 -- Test Data
-node1 = Node 1 "a" 0
+node1 = Node 1 "a" 0 -- 1st node
 node2 = Node 2 "b" (maxBound :: Int)
 node3 = Node 3 "c" (maxBound :: Int)
-node4 = Node 4 "d" (maxBound :: Int)
-
+node4 = Node 4 "d" (maxBound :: Int) -- goal node
+        
+allNodes = [node1,node2,node3,node4]
+        
 edge12 = Edge node1 node2 10
 edge13 = Edge node1 node3 30
 edge24 = Edge node2 node4 40
@@ -59,11 +67,14 @@ findNextNodes :: [Edge] -> Node -> [Node]
 findNextNodes allEdges startNode  =
     map getEndNode $ findEdges allEdges startNode
            
-findNearest :: [Edge] -> Node -> Node
-findNearest allEdges node  =
+findNearestNode :: [Edge] -> Node -> Node
+findNearestNode allEdges node  =
     getEndNode $ minimum $ findEdges allEdges node 
-
-
+               
+findShortestDistanceNode :: [Node] -> Node
+findShortestDistanceNode allNodes =
+    minimum allNodes
+            
 -- 
 main :: IO()
 main = do
